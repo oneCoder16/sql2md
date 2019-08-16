e3571a258a7b6dfb7e282950022f6ec3c042061a
v_gllin <1084910400@qq.com>
2019-08-16 20:32:43 +0800

    test

--- service/parseService.go
+++ service/parseService.go
@@ -1,1 +1,19 @@
 package service
+
+import (
+	"fmt"
+	"regexp"
+	"sql2md/pkg/sql"
+)
+
+type ParseService struct {
+}
+
+//从字符串中解析出 sql
+func (srv *ParseService) Str2Sql(data string) []sql.Table {
+	reg := regexp.MustCompile(`[:print:]`)
+	all := reg.FindAllSubmatch([]byte(data), -1)
+
+	fmt.Printf("%s", all)
+	return nil
+}
---
+++ service/parseService_test.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func Test2Sql(t *testing.T) {
+	sql_file := "../tb_channel.sql"
+
+	file, err := os.Open(sql_file)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+
+	srv := ParseService{}
+	srv.Str2Sql(string(data))
+
+}
